8cc6f7b44d92dd51d2a76b75ea58bee72ac4357a
이철환 <go.lani.developer@gmail.com>
2023-11-09 18:19:28 +0900

    feat: Hooks Sample code update

--- playground/src/Hooks.tsx
+++ playground/src/Hooks.tsx
@@ -4,35 +4,33 @@ import { useEffect } from 'react';
 
 export default function Hooks() {
   const { getCookie, setCookie, hasCookie, deleteCookie } = useCookies();
-  const set = () => {
+  const setTestCookie = () => {
     const day = new Date();
     day.setMinutes(day.getMinutes() + 1);
     setCookie('test', 'true', { expires: 'today' });
   };
 
+  const checkCookie = () => {
+    const hasTest = hasCookie('test');
+    console.log('hasTest', hasTest); // true | false
+  };
+
   useEffect(() => {
-    const test = getCookie('test');
-    console.log('test', test);
+    const testCookie = getCookie('test');
+    console.log('testCookie', testCookie); // true | false
   }, []);
 
   return (
     <main className="flex flex-col">
-      <button
-        type="button"
-        onClick={() => {
-          const hasTest = hasCookie('test');
-          console.log('hasTest', hasTest);
-        }}
-      >
-        check cookie
+      <button type="button" onClick={() => checkCookie()}>
+        Check Cookie
       </button>
-      <button type="button" onClick={set}>
-        set cookie
+      <button type="button" onClick={() => setTestCookie()}>
+        Set Cookie
       </button>
       <button type="button" onClick={() => deleteCookie('test')}>
-        delete cookie
+        Delete Cookie
       </button>
-      hi
     </main>
   );
 }
