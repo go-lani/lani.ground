c2075b6fe5fe9c606715d5b257cd42cf6c4418ed
이철환 <go.lani.developer@gmail.com>
2023-11-09 18:11:33 +0900

    feat: useCookie 추가

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -1,2 +1,3 @@
 export { default as useWindowScroll } from './useWindowScroll';
 export { default as useVisibleElement } from './useVisibleElement';
+export { default as useCookies } from './useCookies';
---
+++ packages/react-hooks/src/useCookies.ts
@@ -0,0 +1,77 @@
+import { useCallback } from 'react';
+
+interface CookieOptions {
+  path?: string;
+  expires?: Date | string | 'today';
+  domain?: string;
+  secure?: boolean;
+  httpOnly?: boolean;
+  maxAge?: number;
+}
+
+export default function useCookies() {
+  const setCookie = useCallback(
+    (name: string, value: string, options: CookieOptions = {}) => {
+      options = {
+        path: '/',
+        ...options,
+      };
+
+      if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+      }
+
+      if (options.expires === 'today') {
+        const today = new Date();
+        today.setHours(24, 0, 0, 0);
+        options.expires = today.toUTCString();
+      }
+
+      let updatedCookie =
+        encodeURIComponent(name) + '=' + encodeURIComponent(value);
+
+      for (let optionKey in options) {
+        const key = optionKey as keyof typeof options;
+
+        updatedCookie += '; ' + key;
+
+        let optionValue = options[key];
+
+        if (optionValue !== true) {
+          updatedCookie += '=' + optionValue;
+        }
+      }
+
+      document.cookie = updatedCookie;
+    },
+    [],
+  );
+
+  const getCookie = useCallback((name: string): string | undefined => {
+    const cookieName = name + '=';
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim();
+      if (cookie.indexOf(cookieName) === 0) {
+        return cookie.substring(cookieName.length);
+      }
+    }
+
+    return;
+  }, []);
+
+  const deleteCookie = (name: string) => {
+    const date = new Date();
+    date.setTime(date.getTime() - 1);
+    setCookie(name, '', {
+      expires: date,
+    });
+  };
+
+  const hasCookie = (name: string): boolean => {
+    return !!getCookie(name);
+  };
+
+  return { setCookie, getCookie, hasCookie, deleteCookie };
+}
