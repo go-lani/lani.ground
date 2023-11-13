3a5337ca0ca4c1277b0d69bf620e789c507e00b7
이철환 <go.lani.developer@gmail.com>
2023-11-13 13:43:31 +0900

    feat: changed event handler(click > mousedown&mouseup)

--- packages/react-outside-click-handler/src/lib/components/index.tsx
+++ packages/react-outside-click-handler/src/lib/components/index.tsx
@@ -1,32 +1,68 @@
-import { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
-type OuterHandlerProps = { onOutsideClick: () => void; children: JSX.Element };
+type OutsideClickHandlerProps = {
+  children: React.ReactNode;
+  onOutsideClick: (e: MouseEvent) => void;
+  disabled?: boolean;
+  capture?: boolean;
+};
 
-export default function OutsideClickHandler({
+const OutsideClickHandler: React.FC<OutsideClickHandlerProps> = ({
   onOutsideClick,
   children,
-}: OuterHandlerProps): JSX.Element {
-  const $outerHandlerRef = useRef<HTMLDivElement>(null);
+  disabled = false,
+  capture = true,
+}) => {
+  const $outerHandlerRef = useRef<HTMLDivElement | null>(null);
 
-  const outsideClickHandler = (e: MouseEvent) => {
-    if (
-      !$outerHandlerRef.current ||
-      $outerHandlerRef.current.contains(e.target as Node)
-    )
-      return;
+  const isInsideContents = (e: MouseEvent) =>
+    $outerHandlerRef.current &&
+    $outerHandlerRef.current.contains(e.target as Node);
 
-    onOutsideClick();
-  };
+  const onMouseUp = useCallback(
+    (e: MouseEvent) => {
+      document.removeEventListener('mouseup', onMouseUp, { capture });
+
+      if (!isInsideContents(e)) {
+        onOutsideClick(e);
+      }
+    },
+    [capture, onOutsideClick],
+  );
+
+  const onMouseDown = useCallback(
+    (e: MouseEvent) => {
+      if (!isInsideContents(e)) {
+        document.addEventListener('mouseup', onMouseUp, { capture });
+      }
+    },
+    [capture, onMouseUp],
+  );
+
+  const addEventListeners = useCallback(() => {
+    if (!disabled) {
+      document.addEventListener('mousedown', onMouseDown, {
+        capture,
+      });
+    }
+  }, [capture, disabled, onMouseDown]);
+
+  const removeEventListeners = useCallback(() => {
+    document.removeEventListener('mousedown', onMouseDown, {
+      capture,
+    });
+    document.removeEventListener('mouseup', onMouseUp, {
+      capture,
+    });
+  }, [capture, onMouseDown, onMouseUp]);
 
   useEffect(() => {
-    document.addEventListener('click', outsideClickHandler, { capture: true });
+    addEventListeners();
 
-    return () => {
-      document.removeEventListener('click', outsideClickHandler, {
-        capture: true,
-      });
-    };
-  }, []);
+    return removeEventListeners;
+  }, [addEventListeners, removeEventListeners]);
 
   return <div ref={$outerHandlerRef}>{children}</div>;
-}
+};
+
+export default OutsideClickHandler;
